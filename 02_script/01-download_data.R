0546ddd18bc46543392f5978f7a319a6f78af8e5
Ben Matthews <bm47@stir.ac.uk>
2025-10-03 14:08:49 +0100

    add in NRS shapefiles from https://www.nrscotland.gov.uk/publications/2022-census-geography-products/

--- 02_script/01-download_data.R
+++ 02_script/01-download_data.R
@@ -14,3 +14,59 @@ unzip(here::here("01_data",
       exdir = here::here("01_data",
                          "raw_data")
 )
+
+
+
+
+# OA lookups --------------------------------------------------------------
+
+oa_lookups <- "https://www.nrscotland.gov.uk/media/utrbt5ze/census_2022_index.zip"
+
+download.file(oa_lookups,
+              destfile = here::here("01_data", 
+                                    "zipfiles",
+                                    "census_2022_oa_lookups.zip"),
+              mode = "wb")
+
+
+
+oa_boundaries_ctc <- "https://www.nrscotland.gov.uk/media/uwdpx4hn/output-area-2022-mhw.zip"
+
+
+download.file(oa_boundaries_ctc,
+              destfile = here::here("01_data", 
+                                    "zipfiles",
+                                    "census_2022_oa_boundaries_ctc.zip"),
+              mode = "wb")
+
+
+
+
+oa_pwc <- "https://www.nrscotland.gov.uk/media/gm4dvdsv/output-area-2022-pwc.zip"
+
+
+download.file(oa_pwc,
+              destfile = here::here("01_data", 
+                                    "zipfiles",
+                                    "census_2022_oa_pwc.zip"),
+              mode = "wb")
+
+
+
+# extract
+
+unzip(here::here("01_data", 
+                 "zipfiles",
+                 "census_2022_oa_boundaries_ctc.zip"),
+      exdir = here::here("01_data",
+                         "raw_data")
+)
+
+
+unzip(here::here("01_data", 
+                 "zipfiles",
+                 "census_2022_oa_pwc.zip"),
+      exdir = here::here("01_data",
+                         "raw_data")
+)
+
